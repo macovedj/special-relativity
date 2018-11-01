3785fe8ab7d759d537273377241e54ff31bbbc1f
Daniel Macovei <djmacovei@gmail.com>
2018-10-31 20:44:16 -0400

    bring needed logic into relativistic clock

--- src/App.js
+++ src/App.js
@@ -19,15 +19,9 @@ class App extends Component {
         setInterval(() =>this.currentTime(), 100);
     }
 
-    handleChange(event) {
-        console.log(event.target.value);
-        this.setState({ speedOfMover: event.target.value });
-    }
-
   render() {
 
       let stationaryTime = this.state.time.toLocaleString().replace(" PM", ":") + this.state.time.getMilliseconds().toString().slice(0,1);
-      let relativisticTime = this.state.time.toLocaleString().replace(" PM", ":") + this.state.time.getMilliseconds().toString().slice(0,1);
 
     return (
       <div className="App">
@@ -36,7 +30,7 @@ class App extends Component {
             Special Relativity Simulator
           </p>
             <Clock time={ stationaryTime }/>
-            <RelativisticClock time={ relativisticTime } speed={ this.state.speedOfMover } changeHandler={this.handleChange} />
+            <RelativisticClock time={ relativisticTime } />
         </header>
       </div>
     );
--- src/Components/RelativisticClock.js
+++ src/Components/RelativisticClock.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const RelativisticClock = (props) => {
+class RelativisticClock extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            lastInc: undefined,
+            stationaryTime: props.time,
+            speedOfMover: 0.0,
+            gamma: 1.0
+        };
 
-    return (
-    <div>
-        <div>I'm a relativisitic clock</div>
-        <p>{props.time}</p>
-        <input id="speedSlider" type="range" min="0" max="0.9" step="0.1" onChange={props.changeHandler}></input>
-        <p>You are travelling at {props.speed || 0} times the speed of light</p>
-    </div>
-    )
+        this.props = props;
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    currentTime() {
+        if (this.state.lastInc === undefined) {
+            this.setState({lastInc: this.props.time});
+        }
+    }
+
+    componentWillMount() {
+        setInterval(() => this.currentTime(), 100);
+    }
+
+    handleChange(event) {
+        this.setState({ speedOfMover: event.target.value });
+        let newGamma = 1.0 / Math.sqrt(1.0 -(Math.pow(this.state.speedOfMover, 2)));
+        this.setState({gamma: newGamma});
+        console.log(this.state);
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div>I'm a relativisitic clock</div>
+                <p>{this.props.time}</p>
+                <input id="speedSlider" type="range" min="0" max="0.9" step="0.1" onChange={this.handleChange}></input>
+                <p>You are travelling at {this.state.speedOfMover || 0} times the speed of light</p>
+            </div>
+        );
+    }
 }
 
 export default RelativisticClock;
